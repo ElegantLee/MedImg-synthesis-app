42e8b38a4159c37f3f82413627dfa475c2201c53
ElegantLee <superlee.handso@gmail.com>
2023-02-25 20:24:04 +0800

    同步修改

--- src/views/Synthesis.vue
+++ src/views/Synthesis.vue
@@ -29,7 +29,17 @@
       <el-row type="flex" justify="space-between">
         <el-col :span="8" :key="t1" :offset="index > 0 ? 2 : 0">
           <el-card :body-style="{ padding: '0px' }">
-            <img src="../assets/images/1_real_A.png" class="image" />
+            <!-- <img src="../assets/images/1_real_A.png" class="image" /> -->
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="image" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
             <div style="padding: 14px;">
               <span>T1 图像</span>
             </div>
@@ -44,7 +54,17 @@
         </el-col>
         <el-col :span="8" :key="t2" :offset="index > 0 ? 2 : 0">
           <el-card :body-style="{ padding: '0px' }">
-            <img src="../assets/images/1_fake_B.png" class="image" />
+            <!-- <img src="../assets/images/1_fake_B.png" class="image" /> -->
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="image" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
             <div style="padding: 14px;">
               <span>T2 图像</span>
             </div>
@@ -61,7 +81,8 @@ export default {
     return {
       ruleForm: {
         model: '',
-        dataset: ''
+        dataset: '',
+        imageUrl: ''
       },
       rules: {
         model: [{ required: true, message: '请选择模型', trigger: 'change' }],
@@ -82,6 +103,21 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
+    },
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      // const isJPG = file.type === 'image/jpeg' || file.type === 'imgage/png';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      // if (!isJPG) {
+      //   this.$message.error('上传头像图片只能是 JPG 格式!');
+      // }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isLt2M;
     }
   }
 };
@@ -130,9 +166,34 @@ export default {
   }
 }
 
+/* 图片上传 */
 .el-divider__text {
   color: #000;
   font-size: 1rem;
   font-weight: bold;
+}
+
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
